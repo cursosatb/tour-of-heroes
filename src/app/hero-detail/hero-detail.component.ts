d7c59e83fd562ddfe5956df38125b890a04debbd
Alejandro <cursosatb@gmail.com>
2022-06-07 17:39:57 +0200

    Mi primer commit

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,44 @@
+import { Location as LocationAngular } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  hero?: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: LocationAngular
+  ) { }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if(id !== 0) {
+      this.heroService.getHero(id)
+        .subscribe(hero => this.hero = hero);
+    }
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
